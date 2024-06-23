c9b9ea72f2cfa2ccd1554d81a6f9e7708dbca8a2
AvikNayak <nayakavik19@gmail.com>
2024-06-23 12:49:29 +0530

    website API creation complete

--- backend/controllers/website.controller.js
+++ backend/controllers/website.controller.js
@@ -43,16 +43,64 @@ const addNewWebsite = async (req, res) => {
 
     res.status(201).json({
       status: true,
-      message: "Website added successfully",
+      message: "Website added successfully!",
       data: newWebsite,
     });
   } catch (error) {
     res.status(500).json({
       status: false,
-      message: "error adding website",
+      message: "Error adding website!",
       error: error.message,
     });
   }
 };
 
-module.exports = { addNewWebsite };
+const deleteWebsite = async (req, res) => {
+  try {
+    const id = req.params.webId;
+
+    if (!id) {
+      res.status(400).json({ status: false, message: "Id is required" });
+      return;
+    }
+
+    const website = await websiteModel.findById(id);
+    if (!website) {
+      res.status(404).json({ status: false, message: "Website not found" });
+      return;
+    }
+
+    await websiteModel.deleteOne({ _id: id });
+
+    res.status(200).json({
+      status: true,
+      message: "Website deleted successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error deleting website!",
+      error: error.message,
+    });
+  }
+};
+
+const getAllWebsites = async (req, res) => {
+  try {
+    const results = await websiteModel.find({ userId: req.user._id });
+
+    res.status(200).json({
+      status: true,
+      message: "All website details fetched successfully!",
+      data: results,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error fetching website details!",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { addNewWebsite, deleteWebsite, getAllWebsites };
--- backend/routes/website.router.js
+++ backend/routes/website.router.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { ensureAuthenticated } = require("../middlewares/auth.middleware");
-const { addNewWebsite } = require("../controllers/website.controller");
+const {
+  addNewWebsite,
+  deleteWebsite,
+  getAllWebsites,
+} = require("../controllers/website.controller");
 
 const router = express.Router();
 
+router.get("/website", ensureAuthenticated, getAllWebsites);
 router.post("/website", ensureAuthenticated, addNewWebsite);
+router.delete("/website/:webId", ensureAuthenticated, deleteWebsite);
 
 module.exports = router;
